ac93e7d6a7cde26e1553f1bb8613fa547f6ebcf4
reflective21 <doublechief6@gmail.com>
2020-03-24 15:43:16 +0100

    added a javascript file

---
+++ index.js
@@ -0,0 +1,12 @@
+
+    var fullName = "David Asiru Adetomiwa";
+    var courses = ["HTML", "CSS", "JAVASCRIPT", "DESIGN"];
+    document.writeln(fullName);
+    var i;
+    num = 201;
+    for (i = 1; i < num; i++) {
+        if ((i % 2) === 0) {
+            document.writeln(i);
+        }
+    }
+
